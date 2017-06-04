00b1186ac6ccc89a6ee56e95facfb53fe8cc4af8
arshdkhn1 <arshdkhn1@gmail.com>
2017-06-04 19:23:58 +0530

    adding update

--- src/calendar-properties-dialog/script.js
+++ src/calendar-properties-dialog/script.js
@@ -1,9 +1,23 @@
 class Wrapper extends React.Component {
   render() {
     return (
-      <div className="wrapper">
-        <label htmlFor="">Enable</label>
-        <input type="checkbox" id=""/>
+      <div className="wrapper dialog-content-box">
+        <div className="calendar-enabler-container row">
+          <input type="checkbox" id="calendar-enabled-checkbox"/>
+          <label htmlFor="calendar-enabled-checkbox">
+            Switch this calendar on
+          </label>
+        </div>
+        <div id="calendar-properties-grid">
+          <div className="calendar-name-row row">
+
+          </div>
+          <div id="calendar-color-row row"></div>
+          <div id="calendar-uri-row row"></div>
+          <div id="calendar-email-identity-row row"></div>
+          <div id="calendar-readOnly-row row"></div>
+          <div id="calendar-suppressAlarms-row row"></div>
+        </div>
       </div>
     );
   }
