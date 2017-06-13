30872238023eead27592a7b24069c605971f1816
arshdkhn1 <arshdkhn1@gmail.com>
2017-06-13 08:56:12 +0530

    added babel remove bash script

--- removeBabelFromHtml.sh
+++ removeBabelFromHtml.sh
@@ -3,3 +3,5 @@ pattern1=' type="text\/babel"'
 pattern2=' src="\/scripts\/babel\.js"'
 echo $pattern2
 sed -i "s/$pattern2//g;s/$pattern1//g" dist/**/*.html
+
+# this script removes `type="text/babel"` and babel scripts from all the index.html files in /dist
