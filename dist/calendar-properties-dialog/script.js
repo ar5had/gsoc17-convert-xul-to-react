467df66385dd0bf68503b948582cefd3585582ee
arshdkhn1 <arshdkhn1@gmail.com>
2017-06-14 19:31:27 +0530

    adding some changes

--- dist/calendar-properties-dialog/script.js
+++ dist/calendar-properties-dialog/script.js
@@ -1,4 +1,252 @@
-class Wrapper extends React.Component {
+// Tab Component
+
+class Tab extends React.Component {
+  componentDidMount() {
+    if (this.props.active) {
+      this.selectTabVisually(this.tab);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.deselectTabVisually(this.tab);
+    if (nextProps.active) {
+      this.selectTabVisually(this.tab);
+    }
+  }
+
+  selectTabVisually(node) {
+    node.setAttribute("visuallyselected", "true");
+    node.setAttribute("selected", "true");
+  }
+
+  deselectTabVisually(node) {
+    node.removeAttribute("visuallyselected");
+    node.removeAttribute("selected");
+  }
+
+  handleTabClick(event) {
+    const { handleTabChange, tabName } = this.props;
+    handleTabChange(tabName);
+  }
+
+  render() {
+    const handleTabClick = this.handleTabClick.bind(this);
+    const { tabName, active } = this.props;
+    return React.createElement(
+      "div",
+      {
+        onClick: handleTabClick,
+        className: `tab ${active ? "selected" : ""}`,
+        id: `${tabName}tab`,
+        key: tabName,
+        ref: node => (this.tab = node)
+      },
+      tabName
+    );
+  }
+}
+
+Tab.propTypes = {
+  handleTabChange: PropTypes.func.isRequired,
+  active: PropTypes.bool.isRequired,
+  tabName: PropTypes.string.isRequired
+};
+
+// TabStrip Component
+
+const TabStrip = ({ tabs, handleTabChange, activeTab }) => {
+  const getTabStripContent = () => {
+    const allTabs = tabs.map(tabName =>
+      React.createElement(Tab, {
+        active: tabName === activeTab,
+        tabName: tabName,
+        key: tabName,
+        handleTabChange: handleTabChange
+      })
+    );
+    return allTabs;
+  };
+
+  const tabStripContent = getTabStripContent();
+
+  return React.createElement("div", { className: "tabStrip" }, tabStripContent);
+};
+
+TabStrip.propTypes = {
+  handleTabChange: PropTypes.func.isRequired,
+  activeTab: PropTypes.string.isRequired,
+  tabs: PropTypes.array.isRequired
+};
+
+// TabPanel Component
+
+const TabPanel = ({
+  data,
+  calendarColorChange,
+  calendarEmailChange,
+  calendarNameChange,
+  calendarToggleChange,
+  calendarUriChange,
+  readOnlyChange,
+  suppressAlarmsChange
+}) => {
+  const getSelectOptions = arr => {
+    arr = arr ? arr : ["NONE"];
+    const options = arr.map((e, i) => React.createElement("option", { value: i, key: i }, e));
+    return options;
+  };
+
+  const { disabled, name, color, uri, readOnly, supressAlarms, emails, selectedEmailIndex } = data;
+
+  const emailOptions = getSelectOptions(emails);
+
+  return React.createElement(
+    "div",
+    { className: "tabContentWrapper" },
+    React.createElement(
+      "div",
+      { id: "calendar-enabler-container", className: "row" },
+      React.createElement("input", {
+        type: "checkbox",
+        className: "checkbox",
+        id: "calendar-enabled-checkbox",
+        value: "disabled",
+        checked: !disabled,
+        onChange: calendarToggleChange
+      }),
+      React.createElement(
+        "label",
+        { htmlFor: "calendar-enabled-checkbox" },
+        "Switch this calendar on"
+      )
+    ),
+    React.createElement(
+      "div",
+      { id: "calendar-properties-grid", className: disabled ? "grid disabled" : "grid" },
+      React.createElement(
+        "div",
+        { id: "calendar-name-row", className: "row" },
+        React.createElement(
+          "label",
+          { htmlFor: "calendar-name", className: "row-label" },
+          "Calendar Name:"
+        ),
+        React.createElement("input", {
+          type: "text",
+          id: "calendar-name",
+          className: "row-input",
+          value: name,
+          onChange: calendarNameChange,
+          disabled: disabled
+        })
+      ),
+      React.createElement(
+        "div",
+        { id: "calendar-color-row", className: "row" },
+        React.createElement(
+          "label",
+          { htmlFor: "calendar-color", className: "row-label" },
+          "Color:"
+        ),
+        React.createElement("input", {
+          type: "color",
+          id: "calendar-color",
+          className: "row-input",
+          value: color,
+          onChange: calendarColorChange,
+          disabled: disabled
+        })
+      ),
+      React.createElement(
+        "div",
+        { id: "calendar-uri-row", className: "row" },
+        React.createElement(
+          "label",
+          { htmlFor: "calendar-uri", className: "row-label" },
+          "Location:"
+        ),
+        React.createElement("input", {
+          type: "text",
+          id: "calendar-uri",
+          className: "row-input",
+          value: uri,
+          onChange: calendarUriChange,
+          disabled: disabled
+        })
+      ),
+      React.createElement(
+        "div",
+        { id: "calendar-email-identity-row", className: "row" },
+        React.createElement(
+          "label",
+          { htmlFor: "email-identity-menulist", className: "row-label" },
+          "E-Mail:"
+        ),
+        React.createElement(
+          "select",
+          {
+            type: "text",
+            id: "email-identity-menulist",
+            className: "row-input hidden",
+            disabled: disabled,
+            onChange: calendarEmailChange,
+            value: selectedEmailIndex
+          },
+          emailOptions
+        )
+      ),
+      React.createElement(
+        "div",
+        { id: "calendar-readOnly-row", className: "row" },
+        React.createElement(
+          "div",
+          null,
+          React.createElement("input", {
+            type: "checkbox",
+            className: "checkbox",
+            id: "readOnly",
+            checked: readOnly,
+            onChange: readOnlyChange,
+            disabled: disabled
+          }),
+          React.createElement("label", { htmlFor: "readOnly" }, "Read Only")
+        )
+      ),
+      React.createElement(
+        "div",
+        { id: "calendar-suppressAlarms-row", className: "row" },
+        React.createElement(
+          "div",
+          null,
+          React.createElement("input", {
+            type: "checkbox",
+            className: "checkbox",
+            id: "fire-alarms",
+            checked: supressAlarms,
+            onChange: suppressAlarmsChange,
+            disabled: disabled
+          }),
+          React.createElement("label", { htmlFor: "fire-alarms" }, "Show Reminders")
+        )
+      )
+    )
+  );
+};
+
+TabPanel.propTypes = {
+  data: PropTypes.object,
+  calendarColorChange: PropTypes.func,
+  calendarEmailChange: PropTypes.func,
+  calendarNameChange: PropTypes.func,
+  calendarToggleChange: PropTypes.func,
+  calendarUriChange: PropTypes.func,
+  readOnlyChange: PropTypes.func,
+  suppressAlarmsChange: PropTypes.func
+};
+
+// DialogContentBox Component
+
+class DialogContentBox extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -45,41 +293,10 @@ class Wrapper extends React.Component {
     setTimeout(() => {
       this.postMessage(JSON.stringify(this.state.tabs, null, 2), `${window.location.origin}`);
     }, 20000);
-
-    // set the visuallyselected attribute of first tab to true
-    const firstTabNode = document.querySelector(".tab");
-    if (firstTabNode) {
-      this.selectTabVisually(firstTabNode);
-    } else {
-      this.tabInterval = setInterval(() => {
-        const firstTab = document.querySelector(".tab");
-        if (firstTab) {
-          this.selectTabVisually(firstTab);
-          clearInterval(this.tabInterval);
-        }
-      }, 50);
-    }
   }
 
   componentWillUnmount() {
     window.removeEventListener("message", this.recieveMessage);
-  }
-
-  selectTabVisually(node) {
-    node.setAttribute("visuallyselected", "true");
-    node.setAttribute("selected", "true");
-  }
-
-  deselectTabVisually(node) {
-    node.removeAttribute("visuallyselected");
-    node.removeAttribute("selected");
-  }
-
-  getSelectOptions(arr) {
-    arr = arr ? arr : ["NONE"];
-    const options = arr.map((e, i) => React.createElement("option", { value: i, key: i }, e));
-
-    return options;
   }
 
   handleCalendarToggleChange(event) {
@@ -128,181 +345,6 @@ class Wrapper extends React.Component {
     this.setState({ tabs: tabsState });
   }
 
-  // remove this tabName arguemnt when tabStrip is done
-  getGeneralTab(tabName) {
-    const {
-      disabled,
-      name,
-      color,
-      uri,
-      readOnly,
-      supressAlarms,
-      emails,
-      selectedEmailIndex
-    } = this.state.tabs[tabName];
-
-    const emailOptions = this.getSelectOptions(emails);
-
-    const handleCalendarToggle = this.handleCalendarToggleChange.bind(this);
-
-    const handleCalendarName = this.handleCalendarNameChange.bind(this);
-
-    const handleCalendarColor = this.handleCalendarColorChange.bind(this);
-
-    const handleCalendarUri = this.handleCalendarUriChange.bind(this);
-
-    const handleCalendarEmail = this.handleCalendarEmailChange.bind(this);
-
-    const handleReadOnly = this.handleReadOnlyChange.bind(this);
-
-    const handleSuppressAlarms = this.handleSuppressAlarmsChange.bind(this);
-
-    return React.createElement(
-      "div",
-      { className: "tabContentWrapper" },
-      React.createElement(
-        "div",
-        { id: "calendar-enabler-container", className: "row" },
-        React.createElement("input", {
-          type: "checkbox",
-          className: "checkbox",
-          id: "calendar-enabled-checkbox",
-          value: "disabled",
-          checked: !disabled,
-          onChange: handleCalendarToggle
-        }),
-        React.createElement(
-          "label",
-          { htmlFor: "calendar-enabled-checkbox" },
-          "Switch this calendar on"
-        )
-      ),
-      React.createElement(
-        "div",
-        { id: "calendar-properties-grid", className: disabled ? "grid disabled" : "grid" },
-        React.createElement(
-          "div",
-          { id: "calendar-name-row", className: "row" },
-          React.createElement(
-            "label",
-            { htmlFor: "calendar-name", className: "row-label" },
-            "Calendar Name:"
-          ),
-          React.createElement("input", {
-            type: "text",
-            id: "calendar-name",
-            className: "row-input",
-            value: name,
-            onChange: handleCalendarName,
-            disabled: disabled
-          })
-        ),
-        React.createElement(
-          "div",
-          { id: "calendar-color-row", className: "row" },
-          React.createElement(
-            "label",
-            { htmlFor: "calendar-color", className: "row-label" },
-            "Color:"
-          ),
-          React.createElement("input", {
-            type: "color",
-            id: "calendar-color",
-            className: "row-input",
-            value: color,
-            onChange: handleCalendarColor,
-            disabled: disabled
-          })
-        ),
-        React.createElement(
-          "div",
-          { id: "calendar-uri-row", className: "row" },
-          React.createElement(
-            "label",
-            { htmlFor: "calendar-uri", className: "row-label" },
-            "Location:"
-          ),
-          React.createElement("input", {
-            type: "text",
-            id: "calendar-uri",
-            className: "row-input",
-            value: uri,
-            onChange: handleCalendarUri,
-            disabled: disabled
-          })
-        ),
-        React.createElement(
-          "div",
-          { id: "calendar-email-identity-row", className: "row" },
-          React.createElement(
-            "label",
-            { htmlFor: "email-identity-menulist", className: "row-label" },
-            "E-Mail:"
-          ),
-          React.createElement(
-            "select",
-            {
-              type: "text",
-              id: "email-identity-menulist",
-              className: "row-input hidden",
-              disabled: disabled,
-              onChange: handleCalendarEmail,
-              value: selectedEmailIndex
-            },
-            emailOptions
-          )
-        ),
-        React.createElement(
-          "div",
-          { id: "calendar-readOnly-row", className: "row" },
-          React.createElement(
-            "div",
-            null,
-            React.createElement("input", {
-              type: "checkbox",
-              className: "checkbox",
-              id: "readOnly",
-              checked: readOnly,
-              onChange: handleReadOnly,
-              disabled: disabled
-            }),
-            React.createElement("label", { htmlFor: "readOnly" }, "Read Only")
-          )
-        ),
-        React.createElement(
-          "div",
-          { id: "calendar-suppressAlarms-row", className: "row" },
-          React.createElement(
-            "div",
-            null,
-            React.createElement("input", {
-              type: "checkbox",
-              className: "checkbox",
-              id: "fire-alarms",
-              checked: supressAlarms,
-              onChange: handleSuppressAlarms,
-              disabled: disabled
-            }),
-            React.createElement("label", { htmlFor: "fire-alarms" }, "Show Reminders")
-          )
-        )
-      )
-    );
-  }
-
-  getTab(tabName) {
-    let Tab;
-    switch (tabName) {
-      case "general":
-        Tab = this.getGeneralTab(tabName);
-        break;
-      default:
-        Tab = this.getGeneralTab(tabName);
-        break;
-    }
-    return Tab;
-  }
-
   postMessage(msg, origin) {
     // parent and window are same thing if the current page is not in any frame
     if (window !== parent) {
@@ -311,20 +353,19 @@ class Wrapper extends React.Component {
   }
 
   recieveMessage(e) {
-    if (e.origin !== window.location.origin) {
+    // react-devtools sends message events whenever you open it so blocking such event is necessary
+    if (e.origin !== window.location.origin || /react-devtools/gi.test(e.data.source)) {
+      console.log(`Blocked request from ${e.origin} with data -`, e.data);
       return;
     }
 
-    this.postMessage(
-      JSON.stringify({ messageRecieved: true }),
-      `${window.location.origin}/iframe-testing-ground`
-    );
+    this.postMessage({ messageRecieved: true }, `${window.location.origin}/iframe-testing-ground`);
 
     console.log("%c Data from Parent: Starts", "color: #333; font-size: 20px; font-weight: bold");
-    console.log(`%c ${e.data}`, "color: #ED4CBC; font-size: 16px");
+    console.log(e.data);
     console.log("%c Data from Parent: Ends", "color: #333; font-size: 20px; font-weight: bold");
 
-    const newTabState = Object.assign({}, JSON.parse(e.data));
+    const newTabState = Object.assign({}, e.data);
     this.setState({ tabs: newTabState });
   }
 
@@ -332,46 +373,45 @@ class Wrapper extends React.Component {
     this.setState({ activeTab: tabName });
   }
 
-  getTabStrip(activeTab) {
-    const tabs = Object.keys(this.state.tabs).map(tabName =>
-      React.createElement(
-        "div",
-        {
-          onClick: event => {
-            const tabNodes = document.querySelectorAll(".tab");
-
-            Array.prototype.forEach.call(tabNodes, tab => {
-              this.deselectTabVisually(tab);
-            });
-
-            this.selectTabVisually(event.target);
-            this.changeTab(tabName);
-          },
-          className: `tab ${activeTab === tabName ? "selected" : ""}`,
-          selected: activeTab === tabName,
-          id: `${tabName}tab`,
-          key: tabName
-        },
-        tabName
-      )
-    );
-
-    if (tabs.length > 1) {
-      return React.createElement("div", { className: "tabStrip" }, tabs);
-    } else {
-      return "";
-    }
-  }
-
   render() {
-    const Tab = this.getTab(this.state.activeTab);
-    const TabStrip = this.getTabStrip(this.state.activeTab);
+    const currentStateData = this.state.tabs[this.state.activeTab];
+    const allTabsName = Object.keys(this.state.tabs);
+    const handleTabChange = this.changeTab.bind(this);
+    const activeTab = this.state.activeTab;
+    const showTabStrip = allTabsName.length > 1;
+    const calendarToggleChange = this.handleCalendarToggleChange.bind(this);
+    const calendarNameChange = this.handleCalendarNameChange.bind(this);
+    const calendarColorChange = this.handleCalendarColorChange.bind(this);
+    const calendarUriChange = this.handleCalendarUriChange.bind(this);
+    const calendarEmailChange = this.handleCalendarEmailChange.bind(this);
+    const readOnlyChange = this.handleReadOnlyChange.bind(this);
+    const suppressAlarmsChange = this.handleSuppressAlarmsChange.bind(this);
+
     return React.createElement(
       "div",
       { className: "wrapper", id: "dialog-content-box" },
-      React.createElement("div", { className: "tabWrapper" }, TabStrip, Tab)
+      React.createElement(
+        "div",
+        { className: "tabPanel" },
+        showTabStrip &&
+          React.createElement(TabStrip, {
+            tabs: allTabsName,
+            handleTabChange: handleTabChange,
+            activeTab: activeTab
+          }),
+        React.createElement(TabPanel, {
+          data: currentStateData,
+          calendarColorChange: calendarColorChange,
+          calendarNameChange: calendarNameChange,
+          calendarToggleChange: calendarToggleChange,
+          calendarUriChange: calendarUriChange,
+          calendarEmailChange: calendarEmailChange,
+          readOnlyChange: readOnlyChange,
+          suppressAlarmsChange: suppressAlarmsChange
+        })
+      )
     );
   }
 }
 
-ReactDOM.render(React.createElement(Wrapper, null), document.getElementById("root"));
+ReactDOM.render(React.createElement(DialogContentBox, null), document.getElementById("root"));
