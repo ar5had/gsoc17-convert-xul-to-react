0fc43cbd7e5ccf369f82897de62dbcc916ec8d3d
arshdkhn1 <arshdkhn1@gmail.com>
2017-06-16 13:03:45 +0530

    adding new build

--- removeBabelFromHtml.sh
+++ removeBabelFromHtml.sh
@@ -5,4 +5,4 @@ pattern1=' type="text\/babel"'
 pattern2=' src="\/scripts\/babel\.js"'
 # pattern3='s/<script type="text\/babel"><\/script>//g'
 # pattern4=s'(<script type="text/babel"></script>(test(g'
-sed -i "s/$pattern2//g;s/$pattern1//g" dist/**/*.html
+sed -i "s/$pattern2//g;s/$pattern1//g" dist/**/**/*.html
