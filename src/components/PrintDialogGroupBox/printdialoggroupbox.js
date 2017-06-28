57ac65fc5fd3e7ac7e72176f4033c7c224820273
arshdkhn1 <arshdkhn1@gmail.com>
2017-06-28 22:57:58 +0530

    adding some updates

--- src/components/PrintDialogGroupBox/printdialoggroupbox.js
+++ src/components/PrintDialogGroupBox/printdialoggroupbox.js
@@ -2,9 +2,24 @@ class PrintDialogGroupBox extends React.Component {
   render() {
     return (
       <div id="groupboxVbox">
-        <FieldSet title="Settings" id="settingsGroup" />
-        <FieldSet title="What to Print" id="what-to-print-group" />
-        <FieldSet title="Options" id="optionsGroup" />
+        <Fieldset title="Settings" id="settingsGroup">
+          <div className="fieldset-content-wrapper">
+            <div className="row">
+              <label htmlFor="title-field" className="row-label">Title:</label>
+              <input className="row-input" type="text" id="title-field" />
+            </div>
+            <div className="row">
+              <label htmlFor="layout-field" className="row-label">Layout:</label>
+              <select className="row-input" id="layout-field">
+                <option value="list">List</option>
+                <option value="monthly-grid">Monthly Grid</option>
+                <option value="weekly-planner">Weekly Planner</option>
+              </select>
+            </div>
+          </div>
+        </Fieldset>
+        <Fieldset title="What to Print" id="what-to-print-group" />
+        <Fieldset title="Options" id="optionsGroup" />
       </div>
     );
   }
