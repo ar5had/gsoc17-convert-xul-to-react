01b149b666cf8261b1247d6b706f2505c8e90659
Arshad Khan <arshdkhn1@gmail.com>
2017-07-02 18:13:48 +0530

    ...

--- dist/components/CalendarAlarmWidget/calendaralarmwidget.js
+++
@@ -1,9 +0,0 @@
-class CalendarAlarmWidget extends React.Component {
-  render() {
-    return React.createElement("div", { className: "calendar-alarm-widget" }, "this is some text;");
-  }
-}
-
-CalendarAlarmWidget.propTypes = {};
-
-window.CalendarAlarmWidget = CalendarAlarmWidget;
--- dist/components/Tabbox/tabbox.js
+++
@@ -1,9 +0,0 @@
-const TabBox = ({ children }) => {
-  return React.createElement("div", { className: "tabbox" }, children);
-};
-
-TabBox.propTypes = {
-  children: PropTypes.array.isRequired
-};
-
-window.TabBox = TabBox;
