62371089118f1aab198d307e7806872ddfd5afaf
Arshad Khan <arshdkhn1@gmail.com>
2017-07-03 00:07:40 +0530

    done with splitter bug

--- dist/components/HSplitter/hsplitter.js
+++ dist/components/HSplitter/hsplitter.js
@@ -10,7 +10,7 @@ class HSplitter extends React.Component {
 
     this.boxDefaultWidth = +window
       .getComputedStyle(this.box)
-      .getPropertyValue("min-width")
+      .getPropertyValue("width")
       .slice(0, -2);
 
     this.splitter.addEventListener("mousedown", e => {
