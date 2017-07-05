f1c1f24911968192673504039e38a2424ef9c267
Arshad Khan <arshdkhn1@gmail.com>
2017-07-05 23:53:15 +0530

    adding some update and comment section for each page

--- scripts/discuss.js
+++ scripts/discuss.js
@@ -1,6 +1,11 @@
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://convert-xul-to-html.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
+var disqus_config = function () {
+this.page.url = window.location.toString().replace(".html", "");
+this.page.identifier = window.location.pathname.slice(1).replace(".html","");
+};
+
+(function() { 
+var d = document, s = d.createElement("script");
+s.src = "https://convert-xul-to-html.disqus.com/embed.js";
+s.setAttribute("data-timestamp", +new Date());
 (d.head || d.body).appendChild(s);
 })();
