145425bd10c935294d4f4d70d3a4aca4f547b99a
Arshad Khan <arshdkhn1@gmail.com>
2017-07-06 00:52:42 +0530

    addin new build

--- scripts/discuss.js
+++ scripts/discuss.js
@@ -1,11 +1,21 @@
-var disqus_config = function () {
-this.page.url = window.location.toString().replace(".html", "");
-this.page.identifier = window.location.pathname.slice(1).replace(".html","");
+const defaultOrigins = ["http://localhost:8000", "http://localhost:9000"];
+let origin = window.location.origin;
+
+if (defaultOrigins.includes(origin)) {
+  origin = "https://gsoc17-convert-xul-to-html.herokuapp.com";
+}
+
+const path = window.location.pathname.replace(".html", "");
+console.log(origin + path, path.slice(1));
+var disqus_config = function() {
+  this.page.url = origin + path;
+  this.page.identifier = path.slice(1);
 };
 
-(function() { 
-var d = document, s = d.createElement("script");
-s.src = "https://convert-xul-to-html.disqus.com/embed.js";
-s.setAttribute("data-timestamp", +new Date());
-(d.head || d.body).appendChild(s);
+(function() {
+  var d = document,
+    s = d.createElement("script");
+  s.src = "https://convert-xul-to-html.disqus.com/embed.js";
+  s.setAttribute("data-timestamp", +new Date());
+  (d.head || d.body).appendChild(s);
 })();
