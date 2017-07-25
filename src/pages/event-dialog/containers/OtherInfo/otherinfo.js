294d04c7c921d7e782128c5d1aec99824a32115d
Arshad Khan <arshdkhn1@gmail.com>
2017-07-26 00:00:06 +0530

    addin cool stuf

--- src/pages/event-dialog/containers/OtherInfo/otherinfo.js
+++ src/pages/event-dialog/containers/OtherInfo/otherinfo.js
@@ -2,19 +2,38 @@
   const { connect } = ReactRedux;
   const { bindActionCreators } = Redux;
 
-  const OtherInfo = ({ state, actions }) => {
-    const {} = state;
-    const {} = actions;
-    // <TabStrip tabs={allTabsName} handleTabChange={handleTabChange} activeTab={activeTab} />
+  class OtherInfo extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeTab: "Description"
+      };
+    }
 
-    return (
-      <div id="other-info-wrapper">
-        <TabBox>
-          <TabStrip tabs={["Description", "Attachment", "Attendees"]} />
-        </TabBox>
-      </div>
-    );
-  };
+    changeTab(tabName) {
+      this.setState({ activeTab: tabName });
+    }
+
+    render() {
+      const handleTabChange = this.changeTab.bind(this);
+
+      const {} = this.props.state;
+      const {} = this.props.actions;
+      // <TabStrip tabs={allTabsName} handleTabChange={handleTabChange} activeTab={activeTab} />
+
+      return (
+        <div id="other-info-wrapper">
+          <TabBox>
+            <TabStrip
+              tabs={["Description", "Attachment", "Attendees"]}
+              activeTab={this.state.activeTab}
+              handleTabChange={handleTabChange}
+            />
+          </TabBox>
+        </div>
+      );
+    }
+  }
 
   OtherInfo.propTypes = {
     state: PropTypes.object.isRequired,
