bdf909fdab3de615428d3e1faeb3b640d738ea41
Arshad Khan <arshdkhn1@gmail.com>
2017-09-02 23:01:29 +0530

    ...slow progress

--- dist/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
+++ dist/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
@@ -10,67 +10,17 @@
         "div",
         { className: "snooze-button-wrapper" },
         React.createElement(
-          "button",
-          { className: "alarm-snooze-button", onClick: this.props.onClick.bind(this) },
-          text
-        ),
-        React.createElement(
-          "div",
-          { className: "snooze-button-dropdown" },
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "5", "data-unit": "M" },
-            "5 Minutes"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "10", "data-unit": "M" },
-            "10 Minutes"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "15", "data-unit": "M" },
-            "15 Minutes"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "30", "data-unit": "M" },
-            "20 Minutes"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "45", "data-unit": "M" },
-            "45 Minutes"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "1", "data-unit": "H" },
-            "1 Hour"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "2", "data-unit": "H" },
-            "2 Hours"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item", "data-value": "2", "data-unit": "H" },
-            "1 Day"
-          ),
-          React.createElement(
-            "div",
-            { "className": "dropdown-item manual-time-setter", "data-value": "2", "data-unit": "H" },
-            React.createElement("input", { type: "number", min: "0" }),
-            React.createElement(
-              "select",
-              { id: "manual-time-unit" },
-              React.createElement("option", { value: "M" }, "minutes"),
-              React.createElement("option", { value: "H" }, "hours"),
-              React.createElement("option", { value: "D" }, "days")
-            ),
-            React.createElement("button", null, "Ok"),
-            React.createElement("button", null, "Cancel")
-          )
+          "select",
+          { className: "alarm-snooze-button", value: "0" },
+          React.createElement("option", { value: "0", disabled: "disabled" }, text),
+          React.createElement("option", { "value": "5", "data-unit": "M" }, "5 Minutes"),
+          React.createElement("option", { "value": "10", "data-unit": "M" }, "10 Minutes"),
+          React.createElement("option", { "value": "15", "data-unit": "M" }, "15 Minutes"),
+          React.createElement("option", { "value": "30", "data-unit": "M" }, "20 Minutes"),
+          React.createElement("option", { "value": "45", "data-unit": "M" }, "45 Minutes"),
+          React.createElement("option", { "value": "1", "data-unit": "H" }, "1 Hour"),
+          React.createElement("option", { "value": "2", "data-unit": "H" }, "2 Hours"),
+          React.createElement("option", { "value": "2", "data-unit": "H" }, "1 Day")
         )
       );
     }
