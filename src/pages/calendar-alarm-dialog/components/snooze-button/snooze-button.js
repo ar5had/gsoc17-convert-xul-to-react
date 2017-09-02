342528f7631c216e3e13f9ffad03a025c42d6563
Arshad Khan <arshdkhn1@gmail.com>
2017-09-02 22:59:42 +0530

    ...slow progress

--- src/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
+++ src/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
@@ -8,18 +8,16 @@
       const text = this.props.type === "all" ? "Snooze All for" : "Snooze for";
       return (
         <div className="snooze-button-wrapper">
-          <button className="alarm-snooze-button" onClick={this.props.onClick.bind(this)}>
-            {text}
-          </button>
-          <select className="snooze-button-dropdown">
-            <option className="dropdown-item" value="5" data-unit="M">5 Minutes</option>
-            <option className="dropdown-item" value="10" data-unit="M">10 Minutes</option>
-            <option className="dropdown-item" value="15" data-unit="M">15 Minutes</option>
-            <option className="dropdown-item" value="30" data-unit="M">20 Minutes</option>
-            <option className="dropdown-item" value="45" data-unit="M">45 Minutes</option>
-            <option className="dropdown-item" value="1" data-unit="H">1 Hour</option>
-            <option className="dropdown-item" value="2" data-unit="H">2 Hours</option>
-            <option className="dropdown-item" value="2" data-unit="H">1 Day</option>
+          <select className="alarm-snooze-button" value="0">
+            <option value="0" disabled="disabled">{text}</option>
+            <option value="5" data-unit="M">5 Minutes</option>
+            <option value="10" data-unit="M">10 Minutes</option>
+            <option value="15" data-unit="M">15 Minutes</option>
+            <option value="30" data-unit="M">20 Minutes</option>
+            <option value="45" data-unit="M">45 Minutes</option>
+            <option value="1" data-unit="H">1 Hour</option>
+            <option value="2" data-unit="H">2 Hours</option>
+            <option value="2" data-unit="H">1 Day</option>
           </select>
         </div>
       );
