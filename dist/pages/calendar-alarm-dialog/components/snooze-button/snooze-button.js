566d234ab296d856c54e329703f544d02ad02b5c
Arshad Khan <arshdkhn1@gmail.com>
2017-09-03 23:52:32 +0530

    addin minor changes

--- dist/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
+++ dist/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
@@ -12,7 +12,7 @@
         React.createElement(
           "select",
           { className: "alarm-snooze-button", value: "0" },
-          React.createElement("option", { value: "0", disabled: "disabled" }, text),
+          React.createElement("option", { value: "NULL", disabled: "disabled" }, text),
           React.createElement("option", { "value": "5", "data-unit": "M" }, "5 Minutes"),
           React.createElement("option", { "value": "10", "data-unit": "M" }, "10 Minutes"),
           React.createElement("option", { "value": "15", "data-unit": "M" }, "15 Minutes"),
@@ -20,7 +20,8 @@
           React.createElement("option", { "value": "45", "data-unit": "M" }, "45 Minutes"),
           React.createElement("option", { "value": "1", "data-unit": "H" }, "1 Hour"),
           React.createElement("option", { "value": "2", "data-unit": "H" }, "2 Hours"),
-          React.createElement("option", { "value": "2", "data-unit": "H" }, "1 Day")
+          React.createElement("option", { "value": "2", "data-unit": "H" }, "1 Day"),
+          React.createElement("option", { value: "CUSTOM" }, "Custom")
         )
       );
     }
