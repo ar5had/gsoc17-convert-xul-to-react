12edda36e11f97ad1a76fa7bf2b4ba49f8794435
Arshad Khan <arshdkhn1@gmail.com>
2017-09-03 23:48:27 +0530

    ...

--- src/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
+++ src/pages/calendar-alarm-dialog/components/snooze-button/snooze-button.js
@@ -9,7 +9,7 @@
       return (
         <div className="snooze-button-wrapper">
           <select className="alarm-snooze-button" value="0">
-            <option value="0" disabled="disabled">{text}</option>
+            <option class="disabled" value="0" disabled="disabled">{text}</option>
             <option value="5" data-unit="M">5 Minutes</option>
             <option value="10" data-unit="M">10 Minutes</option>
             <option value="15" data-unit="M">15 Minutes</option>
@@ -18,6 +18,7 @@
             <option value="1" data-unit="H">1 Hour</option>
             <option value="2" data-unit="H">2 Hours</option>
             <option value="2" data-unit="H">1 Day</option>
+            <option value="CUSTOM">Custom</option>
           </select>
         </div>
       );
