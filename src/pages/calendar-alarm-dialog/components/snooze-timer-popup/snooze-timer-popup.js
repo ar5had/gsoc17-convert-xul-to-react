cba8daad33a0191c2f043797848c2b30adbb79e0
Arshad Khan <arshdkhn1@gmail.com>
2017-09-06 13:34:17 +0530

    done with custom popup

--- src/pages/calendar-alarm-dialog/components/snooze-timer-popup/snooze-timer-popup.js
+++ src/pages/calendar-alarm-dialog/components/snooze-timer-popup/snooze-timer-popup.js
@@ -4,26 +4,60 @@
 
 (function() {
   class SnoozeTimerPopup extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 0,
+        unit: "M"
+      };
+    }
+
+    changeInputValue(event) {
+      const inputValue = parseInt(event.currentTarget.value, 10) || 0;
+      this.setState({
+        value: inputValue
+      });
+    }
+
+    changeTimeUnit(event) {
+      const timeUnit = event.currentTarget.value;
+      this.setState({
+        unit: timeUnit
+      });
+    }
+
+    acceptPopup() {
+      const { value, unit } = this.state;
+      this.props.acceptPopup(value, unit);
+    }
+
     render() {
-      const hidePopup = this.props.hidePopup.bind(this);
+      const closePopup = this.props.closePopup.bind(this);
+      const acceptPopup = this.acceptPopup.bind(this);
+      const changeTimeUnit = this.changeTimeUnit.bind(this);
+      const changeInputValue = this.changeInputValue.bind(this);
+      const inputValue = this.state.value;
+      const timeUnit = this.state.unit;
+
       return (
         <div className="snooze-timer-popup">
           {/* add validation for no < 0  */}
-          <input type="number" min="0" />
-          <select name="time-unit">
-            <option value="MINUTES">minutes</option>
-            <option value="HOURS">hours</option>
-            <option value="DAYS">days</option>
+          <input type="number" min="0" value={inputValue} onChange={changeInputValue} />
+          <select name="time-unit" value={timeUnit} onChange={changeTimeUnit}>
+            <option value="M">minutes</option>
+            <option value="H">hours</option>
+            <option value="D">days</option>
           </select>
-          <span className="snooze-ok-btn" />
-          <span className="snooze-cancel-btn" onClick={hidePopup}/>
+          <span className="snooze-ok-btn" onClick={acceptPopup} />
+          <span className="snooze-cancel-btn" onClick={closePopup} />
         </div>
       );
     }
   }
 
   SnoozeTimerPopup.propTypes = {
-    hidePopup: PropTypes.func.isRequired
+    closePopup: PropTypes.func.isRequired,
+    acceptPopup: PropTypes.func.isRequired
   };
 
   window.SnoozeTimerPopup = SnoozeTimerPopup;
